a07d34779b1b7f31a432431217249481f146866b
Philip CHernonog <chernonogpp@yandex.ru>
2022-07-03 22:19:58 +0300

    init:reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n>0) {
+  return String(n).split('').reverse().map(Number).join('');
+} else if (n<0) {
+    return String(n).split('').reverse().slice(0, -1).map(Number).join('');
+}
 }
